e1e0d82f7b57566ea22ea5a45376db530c213ce1
loli <namolite@foxmail.com>
2023-12-12 18:11:28 +0800

    Feature: Text editor markdown support.

--- renderer/components/context-menu/menu/editorMenu.tsx
+++ renderer/components/context-menu/menu/editorMenu.tsx
@@ -3,26 +3,38 @@ import 'react-contexify/dist/ReactContexify.css'
 
 export default function EditorMenu() {
   const MENU_ID = "editor"
-
-  const matchShortcut = (e: KeyboardEvent) => e.metaKey && e.key === 'f';
+  // TODO Style: Separator text style fix.
+  // TODO Fix: Menu event is null.
+  // TODO Fix: Menu shortcut won't prevent editor shortcut.
+  const matchShortcut = (e: KeyboardEvent) => e.metaKey && e.key === 'b';
 
   return (
     <Menu id={MENU_ID}>
       <Submenu label="Text Formatting">
-        <Item onClick={null} keyMatcher={matchShortcut}>Blod<RightSlot>⌘ + F</RightSlot></Item>
-        <Item onClick={null}>Italic</Item>
-        <Item onClick={null}>Strike</Item>
+        <Item onClick={null} keyMatcher={matchShortcut}>Blod<RightSlot>⌘B</RightSlot></Item>
+        <Item onClick={null}>Italic<RightSlot>⌘I</RightSlot></Item>
+        <Item onClick={null}>Strike<RightSlot>⌥5</RightSlot></Item>
+        <Item onClick={null}>Underline<RightSlot>⌘U</RightSlot></Item>
+        <Item onClick={null}>Highlight<RightSlot>⌘H</RightSlot></Item>
+        <Item onClick={null}>Code<RightSlot>⌘E</RightSlot></Item>
+        <Item onClick={null}>Link<RightSlot>⌘L</RightSlot></Item>
       </Submenu>
-      <Item onClick={null}>Copy</Item>
-      <Item onClick={null}>Paste</Item>
-      <Item onClick={null}>Delete</Item>
-      <Separator />
-      <Item disabled>Disabled</Item>
-      <Separator />
       <Submenu label="Block">
         <Item onClick={null}>Code</Item>
         <Item onClick={null}>Quote</Item>
+        <Item onClick={null}>Divider</Item>
+        <Item onClick={null}>OrderedList</Item>
+        <Item onClick={null}>BulletList</Item>
+        <Item onClick={null}>Table</Item>
       </Submenu>
+      <Separator />
+      <Item onClick={null}>Copy<RightSlot>⌘C</RightSlot></Item>
+      <Item onClick={null}>Paste<RightSlot>⌘V</RightSlot></Item>
+      <Item onClick={null}>Delete<RightSlot>⌘D</RightSlot></Item>
+      <Item onClick={null}>Undo<RightSlot>⌘Z</RightSlot></Item>
+      <Item onClick={null}>Redo<RightSlot>⌘Y</RightSlot></Item>
+      <Separator />
+      <Item disabled>Disabled</Item>
     </Menu>
   )
 }
--- renderer/components/editor/tiptap.js
+++ renderer/components/editor/tiptap.js
@@ -4,34 +4,72 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 // Extensions.
 import Document from '@tiptap/extension-document'
+import History from '@tiptap/extension-history'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
-
+import HardBreak from '@tiptap/extension-hard-break'
 import Typography from '@tiptap/extension-typography'
-// import Highlight from '@tiptap/extension-highlight'
+import CharacterCount from '@tiptap/extension-character-count'
+
 import Heading from '@tiptap/extension-heading'
 import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
 import Strike from '@tiptap/extension-strike'
+import Underline from '@tiptap/extension-underline'
+import Highlight from '@tiptap/extension-highlight'
+import Code from '@tiptap/extension-code'
+import Link from '@tiptap/extension-link'
+
+import OrderedList from '@tiptap/extension-ordered-list'
+import BulletList from '@tiptap/extension-bullet-list'
+import ListItem from '@tiptap/extension-list-item'
 import Blockquote from '@tiptap/extension-blockquote'
+import HorizontalRule from '@tiptap/extension-horizontal-rule'
+import CodeBlock from '@tiptap/extension-code-block'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+
+const limit = 30
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       Document,
+      History,
       Paragraph,
       Text,
+      HardBreak,
+      Typography,
+      CharacterCount.configure({
+        limit,
+      }),
       Heading.configure({
         levels: [1, 2, 3, 4, 5, 6],
       }),
       Bold,
+      Italic,
       Strike,
-      Typography,
+      Underline,
+      Highlight,
+      Code,
+      Link,
+      OrderedList,
+      BulletList,
+      ListItem,
+      Blockquote,
+      HorizontalRule,
+      CodeBlock,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
     ],
     content: '<p>Hello World! 🌎️</p>',
-    autofocus: false,
-    /**
-     *  设置自动获取焦点
-     * 
+    autofocus: false, // 自动聚焦
+    /** 设置自动获取焦点
+     *  
      *  ‘start’: 设置光标在编辑器文档的最前面
      *  ‘end’: 设置光标在编辑器文档的最后面
      *  ‘all’ 选中全部文档
@@ -40,12 +78,23 @@ const Tiptap = () => {
      *  false: 禁用自动获取焦点
      *  null: 禁用自动获取焦点
      */
-    editable: true, // 是否可编辑
-    history: true,  // 启用历史
+    editable: true,   // 是否可编辑
+    history: true,    // 启用历史
   })
 
+  if (!editor) {
+    return null
+  }
+
   return (
-    <EditorContent editor={editor} />
+    <div>
+      <EditorContent editor={editor} />
+      <div className="character-count">
+        {editor.storage.characterCount.characters()}/{limit} characters
+        <br />
+        {editor.storage.characterCount.words()} words
+      </div>
+    </div>
   )
 }
 
