bb05a23cb7e6544128f5bcae90e4991102bccab7
loli <namolite@foxmail.com>
2024-03-06 18:00:29 +0800

    Feature: User Setting.

---
+++ renderer/pages/setting.tsx
@@ -0,0 +1,7 @@
+
+
+import SettingInfo from "./setting/settingInfo"
+
+export default function Setting() {
+  return <>bb</>
+}
---
+++ renderer/pages/setting/settingInfo.tsx
@@ -0,0 +1,5 @@
+
+
+export default function SettingInfo() {
+  return <>SettingInfo</>
+}
