f723a1bf74a5bee7c24b78593d563c40735042de
loli <namolite@foxmail.com>
2024-03-15 15:38:53 +0800

    Update: Server Side Update.

---
+++ renderer/components/editor/index.tsx
@@ -0,0 +1,11 @@
+
+
+
+const Editor = () => {
+  // TODO Open when cooperating only.
+  return <></>
+}
+
+export default Editor
+
+// Yjs
---
+++ renderer/hooks/useAuth.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const useAuth = () => {
+  // 用于检查和更新登录状态的逻辑
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // 可以在这里添加更多逻辑来验证令牌的有效性
+    }
+  }, []);
+
+  const login = async (username, password) => {
+    const url = '/api/login'; // 你的登录API路径
+    try {
+      const response = await axios.post(url, { username, password });
+      const { token } = response.data;
+      localStorage.setItem('authToken', token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // 登录成功后的逻辑
+    } catch (error) {
+      console.error('登录失败', error);
+      // 处理登录失败的逻辑
+    }
+  };
+
+  return { login };
+};
+
+export default useAuth;
